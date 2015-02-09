10417181200de5e0ce673606700c14ca881658c2
jgd <jgd@jgd-ubuntu>
2015-02-09 11:58:05 -0600

    new commiy

---
+++ src/gtikz/GTikz.java
@@ -0,0 +1,23 @@
+package gtikz;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Jesús García Díaz
+ */
+
+public class GTikz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Frame frame = new Frame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Rectangle rect = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+        //mainframe.setSize(rect.width, rect.height);
+        frame.setVisible(true);
+    }
+}
